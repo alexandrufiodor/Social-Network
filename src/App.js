8e2295b8e26bf5f63084cbaf5eddc65f0ce0bd05
Alexandru Fiodor <alexandrufiodor95@gmail.com>
2021-02-22 17:50:00 +0200

    11. Sot seti, verstca, grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="app-wrapper">
+        <header className="header">
+          <img src="https://www.logodesign.net/logo/abstract-waves-with-sun-1353ld.png?size=2&industry=All" alt="logo"/>
+        </header>
+
+        <nav className="nav">
+          <div>
+            <a href="#">Profile</a>
+          </div>
+          <div>
+            <a href="#">Messages</a>
+          </div>
+          <div>
+            <a href="#">News</a>
+          </div>
+          <div>
+            <a href="#">Music</a>
+          </div>
+          <div>
+            <a href="#">Setting</a>
+          </div>
+        </nav>
+        <div className="content">
+          <div>
+            <img src="https://www.etisalat.ae/en/images/ptc-annual-conference-homepage-1920x500_tcm313-157350.jpg" alt=""/>
+          </div>
+          <div>
+            ava + description
+          </div>
+          <div>
+            My posts
+            <div>
+              New post
+            </div>
+            <div>
+              <div>
+                post 1
+              </div>
+              <div>
+                post 2
+              </div>
+              <div>
+                post 3
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
+
 
 export default App;
