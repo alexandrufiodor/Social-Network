8e78bda3e0163eb7f51ec2fc43035f65f65c6658
Alexandru Fiodor <alexandrufiodor95@gmail.com>
2021-02-23 16:39:03 +0200

    12. Разбивка на компоненты

--- src/App.js
+++ src/App.js
@@ -1,57 +1,18 @@
+import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
-const App = () =>  {
-  return (
-      <div className="app-wrapper">
-        <header className="header">
-          <img src="https://www.logodesign.net/logo/abstract-waves-with-sun-1353ld.png?size=2&industry=All" alt="logo"/>
-        </header>
-
-        <nav className="nav">
-          <div>
-            <a href="#">Profile</a>
-          </div>
-          <div>
-            <a href="#">Messages</a>
-          </div>
-          <div>
-            <a href="#">News</a>
-          </div>
-          <div>
-            <a href="#">Music</a>
-          </div>
-          <div>
-            <a href="#">Setting</a>
-          </div>
-        </nav>
-        <div className="content">
-          <div>
-            <img src="https://www.etisalat.ae/en/images/ptc-annual-conference-homepage-1920x500_tcm313-157350.jpg" alt=""/>
-          </div>
-          <div>
-            ava + description
-          </div>
-          <div>
-            My posts
-            <div>
-              New post
-            </div>
-            <div>
-              <div>
-                post 1
-              </div>
-              <div>
-                post 2
-              </div>
-              <div>
-                post 3
-              </div>
-            </div>
-          </div>
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
-      </div>
-  );
+    );
 }
 
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://www.logodesign.net/logo/abstract-waves-with-sun-1353ld.png?size=2&industry=All"
+                 alt="logo"/>
+        </header>
+    )
+}
+
+export default Header;
