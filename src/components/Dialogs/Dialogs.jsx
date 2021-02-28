ae759732a207927d6d8420d12db1ce60310192e0
Alexandru Fiodor <alexandrufiodor95@gmail.com>
2021-02-28 19:13:26 +0200

    21. Dialogs Page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,18 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_item}>
+                <div className={s.dialog + ' ' + s.active}>Alex</div>
+                <div className={s.dialog}>Nicoleta</div>
+                <div className={s.dialog}>Valery</div>
+                <div className={s.dialog}>Dan</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
